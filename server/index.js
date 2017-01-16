d580e6953c9dc029775a42b50965eef7008ea8fd
zjzhome <zjzhome@126.com>
2017-01-16 20:03:45 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,13 @@
+
+/**
+ * 根据客户端传过来的出错的行列
+ * 根据 sourcemap 获取源文件行列
+ *
+ * Inspired by http://www.cnblogs.com/yexiaochai/p/6246490.html
+ */
+
+var sourceMap = require('source-map');
+
+module.exports = function() {
+
+}
