cbbdd79678ea2ff3838245fc3a7fd465a89ab737
zjzhome <zjzhome@126.com>
2017-01-18 10:58:24 +0800

    完善 server 端代码

--- client/index.js
+++ client/index.js
@@ -2,24 +2,24 @@
 (function() {
 
 
-    function uploadErrorMessage(serverAddr) {
+  function uploadErrorMessage(serverAddr) {
 
-    }
+  }
 
-    function structErrorFlag(row, col) {
+  function structErrorFlag(row, col) {
 
-    }
+  }
 
-    /**
-     * 捕捉全局错误
-     * @param  {[type]} errMsg     错误信息
-     * @param  {[type]} url        脚本地址
-     * @param  {[type]} roe        行
-     * @param  {[type]} col        列
-     * @param  {[type]} stack      调用栈
-     */
-    window.onerror = function(errMsg, url, row, col, stack) {
+  /**
+   * 捕捉全局错误
+   * @param  {[type]} errMsg     错误信息
+   * @param  {[type]} url        脚本地址
+   * @param  {[type]} roe        行
+   * @param  {[type]} col        列
+   * @param  {[type]} stack      调用栈
+   */
+  window.onerror = function(errMsg, url, row, col, stack) {
 
-    }
+  }
 
 })();
--- server/index.js
+++ server/index.js
@@ -1,13 +1,51 @@
 
 /**
- * 根据客户端传过来的出错的行列
- * 根据 sourcemap 获取源文件行列
- *
- * Inspired by http://www.cnblogs.com/yexiaochai/p/6246490.html
- */
+* 根据客户端传过来的出错的行列
+* 根据 sourcemap 获取源文件行列
+*
+* Inspired by http://www.cnblogs.com/yexiaochai/p/6246490.html
+*/
+var fs = require('fs')
 
-var sourceMap = require('source-map');
+var sourceMap = require('source-map')
+var SourceMapConsumer = sourceMap.SourceMapConsumer
 
-module.exports = function() {
+function readSourceMapFile(filePath, callback) {
+  var file = fs.createReadStream(filePath)
+  var map = ''
 
+  file.on('data', function(chunk) {
+    map += chunk
+  })
+
+  file.on('end', function() {
+    callback(JSON.parse(map));
+  })
 }
+
+function getOriginalRowCol(map, row, col, callback) {
+  var smc = new SourceMapConsumer(map);
+  var res = smc.originalPositionFor({
+    line: row,
+    column: col
+  })
+  callback(res)
+}
+
+/**
+ * [parseSourceMapFile description]
+ * @param  {[type]} opts [description]
+ *                       filePath
+ *                       row
+ *                       col
+ *                       callback
+ */
+function parseSourceMapFile(opts) {
+  readSourceMapFile(opts.filePath, function(map) {
+    getOriginalRowCol(map, opts.row, opts.col, function(res) {
+      opts.callback && opts.callback(res)
+    })
+  })
+}
+
+module.exports = parseSourceMapFile
